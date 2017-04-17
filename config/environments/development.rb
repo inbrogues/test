fb4ef2899cedddce225b61e44a5ecacfdc3cd34b
SomeSomeone <nice.gl_orl@ukr.net>
2017-04-17 17:14:15 +0300

    USERS & admins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.assets.enabled = true
   config.assets.paths << Rails.root.join('/app/assets/fonts')
+
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
