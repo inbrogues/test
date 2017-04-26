9011b9cbe3a4340cd5bf02517ba0d8834965934e
SomeSomeone <nice.gl_orl@ukr.net>
2017-04-27 00:33:26 +0300

    order

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,8 +6,23 @@ class CartsController < ApplicationController
 		@order.user=current_user
 		@order.address=@address.city+" "+@address.address+" "+@address.seccond_address+" "+@address.post_index
 		puts order_params[:json]
+		#[{"status":"ok","message":"Success!","html":"<b>...</b>","name":"test 3","src":"/system/photos/imgs/000/000/007/original/hmprod-3.jpeg?1493236646","price":3000,"promotional_price":100,"article":"2020","color":"wert","sizes":[],"size":"9","id":7,"count":2}]
+		#"size":"9","id":7,"count":2
+		#,count: nil, product_size_id: nil, product_datum_id: nil
+		data = JSON.parse order_params[:json]
+			OrdersProductDatum
+	    
+
 	    respond_to do |format|
 	      if @order.save
+	      	data.each { |d|
+	      		a=OrdersProductDatum.new
+	      		a.order_id=@order.id
+	      		a.count=data.count
+	      		a.product_size_id=data.id
+	      		a.size=ProductSize.find_by(size: data.size)
+	      		a.save
+	      	}
 	        format.html {redirect_to controller:"carts", action:"order", id:  @order.id , notice: 'Color was successfully created.' }
 	        format.json { render :show, status: :created, location: @order }
 	      else
